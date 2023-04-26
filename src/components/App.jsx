616fde17fb991dcba900d372eea1a89e174e42b5
Vika Semenchuk <vikasemenchuk@Air-Vika.fritz.box>
2023-04-26 23:16:59 +0200

    finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,27 @@
-// import { Component } from 'react';
-import  ContactForm  from './contactForm/ContactForm';
-import { ContactList } from './contactList/ContactList';
-import { Filter } from './filter/Filter';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 
-export class App extends Component {
-  state = {
-    contacts:
-      JSON.parse(localStorage.getItem('contacts')) ??
-      [
-        // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        // { id: 'id-2', name: 'Hermine Kline', number: '443-89-12' },
-        // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ],
-    filter: '',
-  };
+import { ContactForm } from './contactForm/ContactForm';
+import { ContactList } from './contactList/ContactList';
+import { Filter } from './filter/Filter';
 
-  componentDidUpdate(prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      const stringifiedContacts = JSON.stringify(this.state.contacts);
+export const App = () => {
 
-      localStorage.setItem('contacts', stringifiedContacts);
-    }
-  }
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermine Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]
+  );
+  const [filter, setFilter] = useState('');
 
-  addNewContact = data => {
-    const {contacts} = this.state;
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  function addNewContact(data) {
     const newContact = {
       id: nanoid(),
       name: data.name,
@@ -45,61 +39,54 @@ export class App extends Component {
       return;
     }
 
-    this.setState(({ contacts }) => ({
-      contacts: [...contacts, newContact],
-    }));
-  };
+    setContacts(prev => [...prev, newContact]);
+  }
 
-  onChangeFilter = ({ currentTarget: { value } }) => {
-    this.setState({ filter: value });
-  };
-
-  getFilteredContacts = () => {
-    const { filter, contacts } = this.state;
+  function getFilteredContacts() {
     const normalizedContact = filter.toLowerCase();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedContact)
     );
-  };
-
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(({ id }) => id !== contactId),
-    }));
-  };
-
-  render() {
-    const { contacts, filter } = this.state;
-    const filteredContacts = this.getFilteredContacts();
-    return (
-      <>
-        <section className="section">
-          <div className="container">
-            <h1 className="title">Phonebook</h1>
-            <ContactForm addNewContact={this.addNewContact} />
-          </div>
-        </section>
-
-        <section className="section">
-          <div className="container">
-            <h2 className="title">Contacts</h2>
-
-            {contacts.length !== 0 ? (
-              <>
-                <Filter filter={filter} onChangeFilter={this.onChangeFilter} />
-                <ContactList
-                  contacts={filteredContacts}
-                  deleteContact={this.deleteContact}
-                />
-              </>
-            ) : (
-              <>
-                <h5>You still haven't any contacts</h5>
-              </>
-            )}
-          </div>
-        </section>
-      </>
-    );
   }
-}
+
+  function onChangeFilter({ currentTarget: { value } }) {
+    setFilter(value);
+  }
+
+  function deleteContact(contactId) {
+    setContacts(prev => prev.filter(({ id }) => id !== contactId));
+
+    console.log('contacts :>> ', contacts);
+  }
+
+  return (
+    <>
+      <section className="section">
+        <div className="container">
+          <h1 className="title">Phonebook</h1>
+          <ContactForm addNewContact={addNewContact} />
+        </div>
+      </section>
+
+      <section className="section">
+        <div className="container">
+          <h2 className="title">Contacts</h2>
+
+          {contacts.length !== 0 ? (
+            <>
+              <Filter filter={filter} onChangeFilter={onChangeFilter} />
+              <ContactList
+                contacts={getFilteredContacts()}
+                deleteContact={deleteContact}
+              />
+            </>
+          ) : (
+            <>
+              <h5>You still haven't any contacts</h5>
+            </>
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,34 +1,34 @@
 import { useState } from 'react';
-
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 
-export default function ContactForm({ addNewContact }) {
+export function ContactForm({ addNewContact }) {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
   const nameId = nanoid();
   const numberId = nanoid();
+
   const formInfo = { name, number };
 
-  const handleNameChange = ({ currentTarget: { name, value } }) => {
-    setName(() => value);
-  };
+  function handleChange({ currentTarget: { name, value } }) {
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
+  }
 
-  const handleNumberChange = ({ currentTarget: { name, value } }) => {
-    setNumber(() => value);
-  };
+  function reset() {
+    setName('');
+    setNumber('');
+  }
 
-  const reset = () => {
-    setName(() => '');
-    setNumber(() => '');
-  };
-
-  const handleSubmit = e => {
+  function handleSubmit(e) {
     e.preventDefault();
     addNewContact(formInfo);
     reset();
-  };
+  }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -44,7 +44,7 @@ export default function ContactForm({ addNewContact }) {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           value={name}
-          onChange={handleNameChange}
+          onChange={handleChange}
           required
         />
       </div>
@@ -60,7 +60,7 @@ export default function ContactForm({ addNewContact }) {
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           value={number}
-          onChange={handleNumberChange}
+          onChange={handleChange}
           required
         />
       </div>
